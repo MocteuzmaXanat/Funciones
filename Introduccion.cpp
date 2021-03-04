68412cf64bf138d04269121a6687ac5069dce848
MocteuzmaXanat <79545044+MocteuzmaXanat@users.noreply.github.com>
2021-03-03 19:06:51 -0600

    Add files via upload

---
+++ Introduccion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+float a,b,res;
+int main(void)
+
+    {
+        cout<< "\n ingrese el 1er numero\n";
+        cin>> a;
+        cout<<"\n ingrese el 2do numero\n";
+        cin>> b;
+        res=a-b;
+        cout<<"\n La resta es ";
+        cout<< res;
+        cout<< endl;
+        return 0;
+}
